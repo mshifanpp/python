d78d48c34030ceb6bf1661b020d396e2289dde24
mshifanpp <128384863+mshifanpp@users.noreply.github.com>
2025-10-23 11:10:04 +0530

    completed

---
+++ record14/graphics/circles.py
@@ -0,0 +1,5 @@
+def carea(r):
+    print("The area of circle",3.14*r*r)
+def cperi(r):
+    print("the perimeter of circle:",2*3.14*r)
+    
---
+++ record14/graphics/rectangle.py
@@ -0,0 +1,3 @@
+def add(a,b):return a+b
+def rarea(a,b):return a*b
+def rperi(a,b):return 2*(a+b)
